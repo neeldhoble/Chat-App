1f51edf33c9861502af824ba39a657fcf60af08f
neeldhoble <neeldhoble2004@gmail.com>
2025-08-03 18:37:25 +0530

    first commit

--- frontend/src/utils/VerifyUser.jsx
+++ frontend/src/utils/VerifyUser.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
-export const VerifyUser = () => {
+const VerifyUser = () => {
     const {authUser} = useAuth();
     return authUser ? <Outlet/> : <Navigate to={'/login'}/>
 }
+
+export default VerifyUser;
